171888695276f2870cc781fd8da9019962ded1ab
redjin <kpupil@naver.com>
2021-07-07 09:29:59 +0900

    Initial commit

---
+++ app/src/main/java/com/redjin/stopwatch_210707_3/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.redjin.stopwatch_210707_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAdView : AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+    }
+}
